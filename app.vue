fe346d9f10838587d0e64210ba7a1d31a8da5550
devjulianocsa <devjulianocsa@gmail.com>
2024-02-13 08:48:32 -0300

    first commit

---
+++ app.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const client = useMedusaClient();
+const { products } = await client.products.list();
+</script>
+
+<template>
+  <div>
+    {{ products }}
+  </div>
+</template>
+
